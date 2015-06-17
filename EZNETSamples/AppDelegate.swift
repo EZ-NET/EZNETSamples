20ff4c4f26f2c9680c01c39ae5af111a6c39d062
Tomohiro Kumagai <tomohiro@ez-style.jp>
2015-06-17 15:39:00 +0900

    Initial Commit

---
+++ EZNETSamples/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  EZNETSamples
+//
+//  Created by Tomohiro Kumagai on H27/06/17.
+//  Copyright (c) 平成27年 EasyStyle G.K. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ EZNETSamples/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  EZNETSamples
+//
+//  Created by Tomohiro Kumagai on H27/06/17.
+//  Copyright (c) 平成27年 EasyStyle G.K. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
